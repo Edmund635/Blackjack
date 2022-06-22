c43bd560925b6fbf33c5212276fcde08f2b5efc6
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-22 15:17:55 -0400

    Pm update

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+let purseDisplay = document.querySelector("#purse-display")
+let betAmount;
+let betForm = document.querySelector("#bet-form")
+    betForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let betDisplay = document.querySelector("#bet-display")
+    betAmount = document.querySelector("#bet-amount").value
+    betDisplay.textContent = `${betAmount} racks`
+
+    let purseDisplayAmount = purseDisplay.textContent
+    purseDisplayAmount -= betAmount
+    //console.log(purseDisplayAmount)
+    purseDisplay.textContent = `$${purseDisplayAmount}`
+    purseDisplay.textContent = purseDisplayAmount
+    betForm.reset();
+})
+// // if(!betForm.reset()){
+// //     alert("Please place wager!")
+// // }
+// else{
 let deal = document.querySelector('#deal');
 deal.addEventListener('click', function(event){
-fetch('http://deckofcardsapi.com/api/deck/new/draw/?count=4')
+fetch('http://deckofcardsapi.com/api/deck/new/draw/?count=10')
 .then(res => res.json())
 .then(data=> renderCards(data));
 })
@@ -23,18 +43,20 @@ function renderCards(data){
 
     dealer1.src = data.cards[2].image;
     dealer2.src = "https://opengameart.org/sites/default/files/card%20back%20black.png";
-    
-    //  let c1v = data.cards[0]
-    //  let c2v = data.cards[1]
-    //  let c3v = data.cards[2]
-    //  let c4v = data.cards[3]
 
-    //  let cardValues = [c1v, c2v, c3v, c4v]
-
-    //  cardValues.forEach((ele) => faceCardFixer(ele))
     data.cards.forEach((ele) => faceCardFixer(ele))
+    
+     let c1v = data.cards[0]
+     let c2v = data.cards[1]
+     let c3v = data.cards[2]
+     let c4v = data.cards[3]
 
-    //let playerHand = [player1, player2];
+     let cardValues = [c1v, c2v, c3v, c4v]
+
+    cardValues.forEach((ele) => faceCardFixer(ele))
+    
+
+    let playerHand = [player1, player2];
     let pValue = 0;
     
     for(let counter = 0; counter < playerHand.length; counter++){
@@ -44,13 +66,16 @@ function renderCards(data){
     playerCount.textContent = pValue;
 
     let dValue = 0;
-    for(let counter = 2; counter < data.cards.length; counter++){
+    for(let counter = 2; counter < 4; counter++){
+        
         dValue += parseInt(cardValues[counter].value);
     }
     let dealerCount = document.querySelector("#dealerCount")
-    dealerCount.textContent = dValue
+    //dealerCount.textContent = dValue
 
     console.log(dValue)
+
+    
 
     // while(dValue < 17) {
     //     fetch(`http://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`)
@@ -59,10 +84,10 @@ function renderCards(data){
     //         console.log(data)
     //     let newCard = document.createElement('img');
     //     newCard.src = data.cards[0].image;
-    //     let dealerDiv = document.querySelector('#dealer-div');
+    //     let dealerDiv = document.querySelector('#ai-div');
     //     dealerDiv.append(newCard);
 
-    //     faceCardFixer(data.cards[0])
+    //     //faceCardFixer(data.cards[0])
     //     console.log(data.cards[0].value)
     //     dValue += parseInt(data.cards[0].value)
     //     dealerCount.textContent = dValue;
@@ -71,6 +96,7 @@ function renderCards(data){
 
     let hitMe = document.querySelector('#hit-me');
     hitMe.addEventListener('click', function(event){
+        player2.src = data.cards[1].image;
         fetch(`http://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=1`)
         .then(res => res.json())
         .then(data => {
@@ -78,18 +104,49 @@ function renderCards(data){
         newCard.src = data.cards[0].image;
         let playerDiv = document.querySelector('#player-div');
         playerDiv.append(newCard);
+        
 
         faceCardFixer(data.cards[0])
         
         pValue += parseInt(data.cards[0].value)
         playerCount.textContent = pValue;
+        if (pValue > 21) {
+            alert("You busted!!! Click Shuffle")
+        }
         })
     })
+
+    let standBtn = document.querySelector("#stand")
+    standBtn.addEventListener("click", function(event) {
+        while(dValue < 17) {
+            let i = 4
+            let newCard = document.createElement('img');
+            newCard.src = data.cards[i].image;
+            let dealerDiv = document.querySelector('#ai-div');
+            dealerDiv.append(newCard);
+            console.log(data.cards[i].value)
+            dValue += parseInt(data.cards[i].value)
+            
+            ++i
+        }
+    })
+
+    
 
     let revealButton = document.querySelector('.reveal');
     revealButton.addEventListener('click', function(event){
         dealer2.src = data.cards[3].image;
         player2.src = data.cards[1].image;
+        dealerCount.textContent = dValue;
+
+        if(dValue < 22 && dValue >= pValue) {
+                alert("The House always wins! Click Shuffle")
+        } else if (dValue < 22 && pValue > dValue) {
+                alert("You Win!!!")
+                purseDisplay.textContent = `${(betAmount * 2) + purseDisplayAmount}`
+        }else {
+            alert("Dealer busts, You win!")
+        }
     })
 }
 
@@ -102,25 +159,36 @@ function faceCardFixer(ele) {
          return ele.value
   }
 
-let purseDisplay = document.querySelector("#purse-display")
-
-let betForm = document.querySelector("#bet-form")
-betForm.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let betDisplay = document.querySelector("#bet-display")
-    let betAmount = document.querySelector("#bet-amount").value
-    betDisplay.textContent = `${betAmount} racks`
-
-    
-    let purseDisplayAmount = purseDisplay.textContent
-    purseDisplayAmount -= betAmount
-    //console.log(purseDisplayAmount)
-    purseDisplay.textContent = `$${purseDisplayAmount}`
-    betForm.reset();
-
-    purseDisplay.textContent = purseDisplayAmount
-})
+// }
 
 
 
+// create function to return total value of hand 
+// parameters: array of card objects
+// check array to see if there is an ace => filter or find
+// if no ace, just total all cards
+// if there is an ace attempt to total all cards with 11
+// if number goes over 21 total all cards with ace as a 1
 
+
+let nicksHand = [{card: 10, suit: "Diamonds"}, {card: "Ace", suit: "Diamonds"}]
+let edmundsHand = [{card: 8, suit: "Heards"},  {card: 2, suit: "Hearts"}, {card: "Ace", suit: "Hearts"}, {card: "Ace", suit: "clubs"}]
+
+function totalHand(hand){
+    // sort to ace last first
+   return  hand.reduce((cardTotal, card) => {
+    console.log(card)
+    //console.log(cardTotal)
+        if( card.card === "Ace"){
+            if (cardTotal > 10){
+                return cardTotal + 1
+            } else {
+                return cardTotal + 11
+            }
+        } else if (card.card === "Jack"){
+            return cardTotal + 10
+        } else {
+            return cardTotal + card.card
+        }
+    }, 0)
+} 
